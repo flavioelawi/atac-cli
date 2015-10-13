5409b0bc4880d2b34a6a975fde3049aab3f1f53e
Flavio Elawi <flavio.elawi@gmail.com>
2015-10-13 18:28:22 +0200

    Update atac-cli.py

--- atac-cli.py
+++ atac-cli.py
@@ -5,27 +5,14 @@ except ImportError:
   # Python 3 import
   from xmlrpc.client import Server
 
-from pprint import pprint
-
-class bcolors:
-	HEADER = '\033[95m'
-	OKBLUE = '\033[94m'
-	OKGREEN = '\033[92m'
-	YELLOW = '\033[93m'
-	RED = '\033[91m'
-	ENDC = '\033[0m'
-	BOLD = '\033[1m'
-	UNDERLINE = '\033[4m'
+from colorama import Fore
 
 class ataccli:
 
 	def __init__(self):
-		##########INSERT YOUR DEV KEY HERE#############
-		
+		####### INSERT DEV KEY HERE ################
 		DEV_KEY = ''
-		
-		##########INSERT YOUR DEV KEY HERE####################
-		
+		####### INSERT DEV KEY HERE ################
 		s1 = Server('http://muovi.roma.it/ws/xml/autenticazione/1')
 		self.s2 = Server('http://muovi.roma.it/ws/xml/paline/7')
 		self.token = s1.autenticazione.Accedi(DEV_KEY, '')
@@ -50,7 +37,7 @@ class ataccli:
 
 		for i in percorso:
 			res = self.s2.paline.Percorso(self.token, i[0], '', '', '')
-			print(bcolors.YELLOW + "Linea Bus %s capolinea %s" + bcolors.ENDC) % (linea, i[1])
+			print(Fore.YELLOW + "Linea Bus %s capolinea %s" + Fore.RESET) % (linea, i[1])
 			p = 0
 			while p < len(res.get('risposta').get('fermate')):
 				palina = res.get('risposta').get('fermate')[p].get('id_palina')
@@ -61,9 +48,10 @@ class ataccli:
 				except:
 					pass
 				if veicolo:
-					print (bcolors.RED + "%s: %s <-----" + bcolors.ENDC) % (palina,nome)
+					print (Fore.RED + "%s: %s <-----" + Fore.RESET) % (palina,nome)
 				else:
-					print ("%s: %s") % (palina,nome)
+
+					print (Fore.CYAN + "%s: " + Fore.RESET + "%s") % (palina,nome)
 
 				p=p+1
 
